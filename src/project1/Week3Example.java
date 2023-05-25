f5390c6a715efce6e780e4576ad2fbba2c40f151
Krish2128 <parmkris@sheridancollege.ca>
2023-05-25 15:25:47 -0400

    branch version 1

--- src/project1/Week3Example.java
+++ src/project1/Week3Example.java
@@ -11,6 +11,7 @@ public class Week3Example {
     public static void main(String[] args)
     {
         System.out.println("learning github operations");
+        System.out.println("learning github operations");
     }
     
 }
